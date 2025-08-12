742c6bf9c7498549eb34050fead5465a6fbfbbf7
lkoaiza-abdelkarim <karimlkoaiza6@gmail.com>
2025-08-12 12:00:55 +0100

    update exercices

--- week_1/day_1/exercises/exercises_xp_gold.py
+++ week_1/day_1/exercises/exercises_xp_gold.py
@@ -1,15 +1,52 @@
 # ------ Exercise 1
-
+month= int(input("Quel mois de l'année ? (1-12) "))
+if 3 <= month <= 5:
+    print("spring")
+elif 6 <= month <= 8:
+    print("summer")
+elif 9 <= month <= 11:
+    print("autumn")
+else:
+    print("winter")
 # ------ Exercise 2
-
+for i in range(1, 21):
+    print(i, end=" ")
+print()
+for i in range(1, 21,2):
+    print(i+1, end=" ")
 # ------ Exercise 3
+name = input("Quel est votre nom ? ")
+while name==karim:
+    name = input("Quel est votre nom ? ")
 
 # ------ Exercise 4
+names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
 
+for i in range(len(names)):
+    if names[i] in names:
+        print(i)
+    else:
+        print("Ce nom n'est pas dans la liste.")
 # ------ Exercise 5
+x= int(input("Entrez un nombre entier : "))
+y= int(input("Entrez un autre nombre entier : "))
+z= int(input("Entrez un troisième nombre entier : "))
+print(f"Le plus grand nombre est {max(x, y, z)}")
+#methode 2
+x= int(input("Entrez un nombre entier : "))
+y= int(input("Entrez un autre nombre entier : "))
+z= int(input("Entrez un troisième nombre entier : "))
+if x > y and x > z:
+    print(f"Le plus grand nombre est {x}")
 
 # ------ Exercise 6
-
+import random
+x=int(input("Entrez un nombre entier  entre 1 et 9: "))
+y=random.randint(1,9)
+if x == y :
+    print("Vous avez gagné !")
+else:
+    print(f"Vous avez perdu ! Le nombre était {y}")
 # ------ Exercise 7
 
 # ------ Exercise 8
--- week_1/day_1/exercises/exercises_xp_ninja.py
+++ week_1/day_1/exercises/exercises_xp_ninja.py
@@ -1,9 +1,30 @@
 # ------ Exercise 1
 
+
+
+
 # ------ Exercise 2
+mot= input("Entrez un mot : ")
+
+y=''
+while 'A' not in y and len(mot) > len(y):
+    print("félicitations", end=" ")
+    y=mot
+    x = input("Entrez un mot : ")
 
 # ------ Exercise 3
+x='''L’intelligence artificielle (IA) transforme la manière dont les humains interagissent avec la technologie. Des assistants vocaux aux voitures autonomes, en passant par les diagnostics médicaux avancés, l’IA est désormais présente dans presque tous les aspects de la vie moderne. Elle permet aux machines d’apprendre à partir de données, de reconnaître des motifs et de prendre des décisions avec une intervention humaine minimale. À mesure que l’IA continue d’évoluer, elle apporte à la fois des opportunités et des défis, soulevant d’importantes questions sur l’éthique, la vie privée et l’avenir du travail.'''
 
+i=0
+while y in x and y=='.':
+    i+=1
+print(i, end=" ")
+
+split = x.split()
+print("Nombre de mots :", len(x))
+
+y = set(x)
+print("Nombre de mots :", len(y))
 # ------ Exercise 4
 
 # ------ Exercise 5
