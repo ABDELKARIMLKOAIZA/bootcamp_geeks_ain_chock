3a96882e46b9d67b146c7487d564c02971b7c2ea
lkoaiza-abdelkarim <karimlkoaiza6@gmail.com>
2025-08-13 17:21:26 +0100

    day2

--- week_1/day_2/exercises/exercises_xp_gold.py
+++ week_1/day_2/exercises/exercises_xp_gold.py
@@ -1,15 +1,63 @@
 # ------ Exercise 1
+def date(a,b,c):
+    return f"{a}/{b}/{c}"
 
+
+
+
+
+def util():
+    birthdays={'karim':date(12,6,2000)  ,
+           'zakaria': date(28,8,2005)  ,
+           'youness':date(11,4,2002)   ,
+           'ahmed':date(6,7,2003)  ,
+           'yassine': date(6,7,2004) 
+          }
+    print('Vous pouvez rechercher les dates d’anniversaire des personnes de la liste !')
+    nom= input('donner le nom d’une personne')
+    x=birthdays[nom]
+    return f"La date d'anniv de {nom} est {x}"
+
+print(util())
 # ------ Exercise 2
+def util1():
+    birthdays={'karim':date(12,6,2000)  ,
+           'zakaria': date(28,8,2005)  ,
+           'youness':date(11,4,2002)   ,
+           'ahmed':date(6,7,2003)  ,
+           'yassine': date(6,7,2004) 
+          }
+    print('Vous pouvez rechercher les dates d’anniversaire des personnes de la liste !')
+    print(list( birthdays.keys()))
+        
+    nom= input('donner le nom d’une personne')
+    if nom not in birthdays.keys():
+        print(f"Désolé, nous n’avons pas les informations d’anniversaire pour {nom}")
+    else:
+        x=birthdays[nom]
+        return f"La date d'anniv de {nom} est {x}"
 
+print(util1())
 # ------ Exercise 3
+def throw_dice ():
+    nombre = random.randint(1, 6)
+    return nombre
+def throw_until_doubles(a=throw_dice (),b=throw_dice ()):
+    l=[]
+    i=0
+    while a!=b :
+        a=throw_dice ()
+        b=throw_dice ()
+        l.append((a,b))
+        i+=1
+    return l , i
 
-# ------ Exercise 4
+def main():
+    l=[]
+    for i in range(100):
+        l.append(throw_until_doubles())
+    return l 
 
-# ------ Exercise 5
 
-# ------ Exercise 6
 
-# ------ Exercise 7
 
-# ------ Exercise 8
--- week_1/day_2/exercises/exercises_xp_ninja.py
+++ week_1/day_2/exercises/exercises_xp_ninja.py
@@ -1,6 +1,34 @@
 # ------ Exercise 1
+# Chaîne initiale
+cars_str = "Volkswagen, Toyota, Ford Motor, Honda, Chevrolet"
+
+
+cars_list = cars_str.split(", ")
+
+
+print(f"Il y a {len(cars_list)} fabricants dans la liste.")
+
+
+print("Liste en ordre décroissant :", sorted(cars_list, reverse=True))
+
+
+count_o = len([car for car in cars_list if 'o' in car.lower()])
+print(f"{count_o} fabricants ont la lettre 'o' dans leur nom.")
+
+
+count_no_i = len([car for car in cars_list if 'i' not in car.lower()])
+print(f"{count_no_i} fabricants n'ont pas la lettre 'i' dans leur nom.")
 
 # ------ Exercise 2
+def get_full_name(first_name, last_name, middle_name=None):
+    if middle_name:  
+        return f"{first_name.capitalize()} {middle_name.capitalize()} {last_name.capitalize()}"
+    else:  
+        return f"{first_name.capitalize()} {last_name.capitalize()}"
+
+print(get_full_name(first_name="john", middle_name="hooker", last_name="lee"))
+
+print(get_full_name(first_name="bruce", last_name="lee"))
 
 # ------ Exercise 3
 
