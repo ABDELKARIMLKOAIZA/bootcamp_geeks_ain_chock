0bc92fb071eb39e672bd90b56916e228f14d90a7
ABDELKARIMLKOAIZA <karimlkoaiza6@gmail.com>
2025-09-02 18:08:30 +0100

    Update Daily challenge .sql

--- week2/day1/day2/Daily challenge .sql
+++ week2/day1/day2/Daily challenge .sql
@@ -1,1 +1,19 @@
+-- Q1: SELECT COUNT(*)
+--     FROM FirstTab AS ft 
+--     WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL );
+-- Réponse: 3
 
+-- Q2: SELECT COUNT(*)
+--     FROM FirstTab AS ft 
+--     WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 );
+-- Réponse: 1
+
+-- Q3: SELECT COUNT(*)
+--     FROM FirstTab AS ft 
+--     WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
+-- Réponse: 0
+
+-- Q4: SELECT COUNT(*)
+--     FROM FirstTab AS ft 
+--     WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );
+-- Réponse: 1
