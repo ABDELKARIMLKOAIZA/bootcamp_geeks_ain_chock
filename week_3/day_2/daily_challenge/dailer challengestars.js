4afae06fe3620e587ebacb99777da556ea008777
ABDELKARIMLKOAIZA <karimlkoaiza6@gmail.com>
2025-09-12 12:52:17 +0100

    Update dailer challengestars.js

--- week_3/day_2/daily_challenge/dailer challengestars.js
+++ week_3/day_2/daily_challenge/dailer challengestars.js
@@ -1,1 +1,16 @@
+const n = 6;
+
+// 1) Une seule boucle
+let row = "";
+for (let i = 1; i <= n; i++) {
+  row += "* ";
+  console.log(row.trim());
+}
+
+// 2) Deux boucles imbriquées
+for (let i = 1; i <= n; i++) {
+  let line = "";
+  for (let j = 1; j <= i; j++) line += "* ";
+  console.log(line.trim());
+}
 
